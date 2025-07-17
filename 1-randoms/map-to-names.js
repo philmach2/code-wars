648683f384204636e9ce34244605815b6cfc4666
Phil Mach <110493891+philmach2@users.noreply.github.com>
2025-07-17 14:07:18 -0400

    obj

---
+++ 1-randoms/map-to-names.js
@@ -0,0 +1,22 @@
+// Map to names
+
+// You have an array of user objects, each one has user.name. Write the code that converts it into an array of names.
+
+// For instance:
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let users = [john, pete, mary];
+
+let names = users.map((item) => item.name);
+
+console.log(names); // John, Pete, Mary
+
+// inputs : array of user objects
+// output : array of name strings
+// steps :
+//  iterate through array of user objects
+//  .map()
+//  transform array by making each item in new array users.name
